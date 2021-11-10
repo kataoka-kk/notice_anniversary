df43f65ce272b3412ea9a44e91282476ee79af06
kataoka-kk <horeizai.1201@gmail.com>
2021-11-10 18:18:29 +0900

    Update ListPage.dart
    tile scroll issue

--- lib/views/screens/ListPage.dart
+++ lib/views/screens/ListPage.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notice_anniversary/viewmodels/ListViewModel.dart';
 import 'package:notice_anniversary/views/components/AnniversaryTile.dart';
+import 'package:provider/provider.dart';
 
 class ListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final viewModel = context.read<ListViewModel>();
+
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.pink.shade50,
@@ -14,24 +18,36 @@ class ListPage extends StatelessWidget {
           onPressed: null,
         ),
         body: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(2.0),
-                    child: AnniversaryTile(
-                      month: 12,
-                      day: 1,
-                      tag: "誕生日",
-                      title: "片岡の誕生日",
-                    ),
-                  ),
-                ],
-              ),
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Column(
+              children: <Widget>[
+                Consumer<ListViewModel>(builder: (context, model, child) {
+                  return listViewBuilder(model);
+                }),
+              ],
             ),
           ),
+        ),
+      ),
+    );
+  }
+
+  Widget listViewBuilder(ListViewModel model) {
+    //TODO: 一旦複数明細かつ、切り出しを行えた
+
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: 10,
+      // itemCount: model.articles.length,
+      itemBuilder: (context, int position) => Padding(
+        padding: const EdgeInsets.all(2.0),
+        child: AnniversaryTile(
+          month: 12,
+          day: 1,
+          tag: "誕生日",
+          title: "片岡康希の誕生日",
         ),
       ),
     );
